ee3d9a11fd5de9eea855fd617ce60a2117ed117e
MetalGrey <37623584+MetalGrey@users.noreply.github.com>
2024-06-29 22:22:08 +0900

    https

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
+const https = require('https');
 
 const app = express();
 const port = 3000;
 
+// Middleware для обработки данных из форм и JSON
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Обработчик маршрута для сохранения логов
 app.post('/logs', (req, res) => {
     const logMessage = req.body.log;
     if (logMessage) {
@@ -25,6 +28,24 @@ app.post('/logs', (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Log server listening at http://localhost:${port}`);
+// Обработчик маршрута для выдачи файла из .well-known/pki-validation
+app.get('/.well-known/pki-validation/.txt', (req, res) => {
+    const filePath = path.join(__dirname, '..', '..', '.txt');
+    res.sendFile(filePath);
 });
+
+// Создание HTTPS сервера с помощью SSL сертификата и ключа
+const httpsOptions = {
+    key: fs.readFileSync('Rythmic-Cube-Log-Server/private.key'),
+    cert: fs.readFileSync('Rythmic-Cube-Log-Server/certificate.crt'),
+    ca: fs.readFileSync('Rythmic-Cube-Log-Server/ca_bundle.crt')
+};
+
+const httpsServer = https.createServer(httpsOptions, app);
+
+// Запуск HTTPS сервера
+httpsServer.listen(port, () => {
+    console.log(`Server listening at https://localhost:${port}`);
+});
+
+
